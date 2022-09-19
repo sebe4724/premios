a5dd90bfb6239b28692168e49a3a0227ddabbf7b
Sergio B <sebe4724@gmail.com>
2022-09-19 14:22:07 -0500

    Se agregaron estilos y parte del front de la app

--- premiosplatziapp/polls/admin.py
+++ premiosplatziapp/polls/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
-from .models import Question
+from .models import Choices, Question
+
+
+class ChoiceInLine(admin.StackedInline):
+    model = Choices
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ["pub_date", "question_text"]
+    inlines = [ChoiceInLine]
+    list_display = ("question_text","pub_date", "was_published_recently")
+    list_filter = ["pub_date"]
 
 
 admin.site.register(Question)
