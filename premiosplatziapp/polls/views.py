578fc77601e139ec1f292b5e28fc5212ae5d2f0e
Sergio B <sebe4724@gmail.com>
2022-09-19 05:31:45 -0500

    Segundo commit, segunda actualizacion

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,8 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hola")
+    latest_question_list = Question.objects.all()
+    return render(request, "polls/index.html", {
+        "latest_question_list": latest_question_list
+    })
 
-# Create your views here.
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {
+        "question": question
+
+    })
+
+
+def results(request, question_id):
+    return HttpResponse(f"Estas viendo los resultados de la pregunta numero {question_id}")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"Estas votando la pregunta numero {question_id}")
